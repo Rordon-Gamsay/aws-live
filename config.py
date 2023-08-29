f499fdadaf8053fda51791dea992c7352227d57e
Goh Ze Han <86219912+Rordon-Gamsay@users.noreply.github.com>
2023-08-29 15:40:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cbqeusbegrsk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "gohzehan-employee"
+customregion = "us-east-1"
+
